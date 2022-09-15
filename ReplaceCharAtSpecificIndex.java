8c169b99b3f5123a0ef9c360feefc078f3e9e1e9
arao1209 <aneri.ami12@gmail.com>
2022-09-14 17:56:13 -0700

    New StringBuffer concept

--- ReplaceCharAtSpecificIndex.java
+++ ReplaceCharAtSpecificIndex.java
@@ -2,6 +2,30 @@ package stringConcept;
 
 public class ReplaceCharAtSpecificIndex {
 
+    public static void main(String[] args) {
 
+        String word = "Tirth";
+
+        ReplaceCharAtSpecificIndex replaceCharAtSpecificIndex = new ReplaceCharAtSpecificIndex();
+        replaceCharAtSpecificIndex.replaceChar(word);
+    }
+
+    private void replaceChar(String word){
+
+        int index = 3;
+        char ch = 'a';
+
+        System.out.println("Original Word: "+word);
+
+//        word = word.substring(0,3)+ch+word.substring(4);
+//
+//        System.out.println("New Word": +word);
+
+        StringBuffer stringBuffer = new StringBuffer(word);
+        stringBuffer.setCharAt(0, 'A');
+        System.out.println("New Word: " +stringBuffer);
+
+
+    }
 
 }
